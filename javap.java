ec5d69ada86fe272521acfa79f2c424a065ef6f0
Ravindra-DevOps <thanishinfotech01@gmail.co>
2020-02-20 08:47:28 +0530

    dfd

---
+++ javap.java
@@ -0,0 +1,24 @@
+class javap
+{
+public static void main(String[] args)
+{
+int a,b,c,d;
+
+a=60;
+b=400;
+c=20;
+d=100;
+if(a>b && a>c && a>d)
+System.out.println("The biggest value is A"+a);
+else
+if (b>a && b>c && b>d)
+System.out.println("THe Biggest values is B"+b);
+else
+if(c>a && c>b && c>d)
+System.out.println("The Biggest value is c"+c);
+else
+if(d>a && d>b && d>c)
+System.out.println("The Bigges value is d "+d);
+}
+}
+
